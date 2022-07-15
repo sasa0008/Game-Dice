6c78172ea8206056a645e092980a5380c4909bf7
sasa0008 <108990277+sasa0008@users.noreply.github.com>
2022-07-15 17:27:26 +0530

    Fixed

--- index.js
+++ index.js
@@ -1,27 +1,17 @@
+var p1=prompt("Enter Player1 Name ");
+var p2=prompt("Enter Player2 Name ");
 
-function requiredFunction1() {
-    var p1=prompt("Enter Player1 Name ");
-    if (p1 == "") {
-      requiredFunction1();
-    }
-  }
-  requiredFunction1();
-
-function requiredFunction2() {
-    var p2=prompt("Enter Player2 Name ");
-    if (p2 == "") {
-      requiredFunction2();
-    }
-  }
-  requiredFunction2();
 
 var p1_dice=document.getElementById('pla_1');
 if (p1==null) {p1="Player 1"}
+else if (p1==""){p1="Player 1"}
 else p1_dice.innerHTML=p1;
+console.log(p1)
 
 var p2_dice=document.getElementById('pla_2');
 //console.log(p2_dice);
 if (p2==null) {p2="Player 2"}
+else if(p2=='') {p2="Player 2"}
 else p2_dice.innerHTML=p2;
 
 var randomNumber1=Math.floor((Math.random())*6)+1;
